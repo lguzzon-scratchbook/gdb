70520e56ba31b9769b5cb42a01f39533519c32c0
estebanrfp <estebanrfp@gmail.com>
2025-09-08 04:11:25 +0200

    update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,19 @@
-export default {
-  env: {
-    browser: true,
-    node: true,
-    es6: true
-  },
-  extends: [
-    'standard'
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-    ecmaVersion: 11,
-    sourceType: 'module'
-  },
-  rules: {
-    // 'no-tabs': ['error', { allowIndentationTabs: true }],
-    // 'arrow-body-style': ['error', 'as-needed'],
-    'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
-    'prefer-template': 'error'
-    // 'template-curly-spacing': ['error', 'always'],
-    // 'arrow-parens': ['error', 'as-needed'],
-    // 'no-return-assign': ['error', 'always'],
-    // 'no-use-before-define': ['error', { functions: true, classes: true }],
-    // camelcase: 'off'
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 11,
+      sourceType: 'module',
+      globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+        browser: true,
+        node: true,
+        es6: true
+      }
+    },
+    rules: {
+      'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+      'prefer-template': 'error'
+    }
   }
-}
+];
